b58e370355696bac81ce3cc79dc6fe1d049c5787
dudelectric <dudelectric@gmail.com>
2015-02-13 23:14:02 +0700

    second

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150213155449) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "published_at"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "excerpt"
+    t.string   "location"
+  end
 
 end
