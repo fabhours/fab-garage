9fe65064138a2114c21ca1b0a82d6e05ded11132
dudelectric <dudelectric@gmail.com>
2015-03-01 14:40:29 +0700

    until page 113

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150219094222) do
+ActiveRecord::Schema.define(version: 20150301070842) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,27 @@ ActiveRecord::Schema.define(version: 20150219094222) do
     t.datetime "updated_at",   null: false
     t.string   "excerpt"
     t.string   "location"
+    t.integer  "user_id"
+  end
+
+  create_table "articles_categories", id: false, force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "article_id"
+    t.string   "name"
+    t.string   "email"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "profiles", force: :cascade do |t|
