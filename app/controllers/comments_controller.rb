39f21ef6995dc833a554f2490c7a45804859ce1e
dudelectric <dudelectric@gmail.com>
2015-03-29 14:56:29 +0700

    add ajax calls

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,9 +5,17 @@ class CommentsController < ApplicationController
 	def create
 		@comment = @article.comments.new(comment_params)
 		if @comment.save
-			redirect_to @article, notice: 'Thanks for your comment'
+			respond_to do |format|
+				format.html { redirect_to @article, notice: 'Thanks for your comment' }
+				format.js#this block will render create.js.erb 
+			end
+			# redirect_to @article, notice: 'Thanks for your comment'
 		else
-			redirect_to @article, alert: 'Unable to add comment'
+			respond_to do |format|
+				format.html { redirect_to @article, alert: 'Unable to add comment' }
+				format.js { render 'fail_create.js.erb' }
+			end
+			# redirect_to @article, alert: 'Unable to add comment'
 		end
 	end
 	
@@ -15,7 +23,10 @@ class CommentsController < ApplicationController
 		@article = current_user.articles.find(params[:article_id])
 		@comment = @article.comments.find(params[:id])
 		@comment.destroy
-		redirect_to @article, notice: 'Comment Deleted'
+		respond_to do |format|
+			format.html{redirect_to @article, notice: 'Comment Deleted'}
+			format.js
+		end
 	end
 	
 	private
